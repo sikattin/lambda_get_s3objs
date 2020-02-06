0385fdbf3a21c3e6f93590d913b4fa40106f10b8
shikano.takeki <distortion0mix@gmail.com>
2020-02-06 17:55:04 +0900

    refuctoring

--- lambda/get_s3objects.py
+++ lambda/get_s3objects.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
+import time
 import boto3
 import logging
 from botocore.exceptions import BotoCoreError
@@ -40,44 +41,99 @@ class S3Client(object):
         del paginator
         del resp_iter
         return res
-
+        
+def check_size(src, dest):
+    if not isinstance(src, int) or not isinstance(dest, int):
+        raise ValueError('must be int')
+        
+    if src == dest:
+        return 'OK'
+    else:
+        return 'NG'
+        
+def get_s3objsiter(bucket, prefix=None):
+    if prefix is None:
+        prefix = ''
+    resp_iter = paginator.paginate(
+        Bucket=bucket,
+        Prefix=prefix
+    )
+    return resp_iter
+    
+def construct_mailbody(iter):
+    """construct mailbody
+    
+    Args:
+        iter (botocore.paginate.PageIterator): PageIterator object
+    
+    Returns:
+        str: mail body
+    """
+    global bucket
+    global s3client
+    key = str()
+    size = int()
+    result = str()
+    for ele in iter:
+        if ele['Contents']:
+            for d in ele['Contents']:
+                chksize_res = 'Not defined'
+                key = d.get('Key')
+                size = int(d.get('Size'))
+                res = s3client.get_object(Bucket=bucket, Key=key)
+                if 'Metadata' in res and 'src_size' in res['Metadata']:
+                    chksize_res = check_size(int(res['Metadata']['src_size']), size)
+                result += "{0} {1} {2}\n".format(key, size, chksize_res)
+        else:
+            result += "!!! Not found objects in S3 bucket. " \
+            "Check S3 bucket then make sure that backup source has been transfered correctly. !!!"
+    return result
+    
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+bucket = str()
+s3client = boto3.client('s3')
+paginator = s3client.get_paginator('list_objects_v2')
 result = dict()
 
 def lambda_handler(event, context):
+    global bucket
+    global s3client
+    global paginator
+    fileter_by_prefixes = list()
+
     bucket = event['bucket']
-    fileter_by_prefixes = event['filter']
-    mail_body = 'Bucket: {}\n\n'.format(bucket)
+    if 'filter' in event:
+        # list of filters to get/list objects.
+        fileter_by_prefixes = event['filter']
+    body_header = "Bucket: {}\n\n".format(bucket)
+    body_header += 'Key Size(bytes) Result\n'
+    body_header += '-----------------------------'
     try:
-        s3client = boto3.client('s3')
         paginator = s3client.get_paginator('list_objects_v2')
     except BotoCoreError as e:
         logger.exception('raised unexpected error while initializing s3 uploader client.')
         logger.error(str(e))
         raise e
-    else:
+    if fileter_by_prefixes:
         # get s3 objects
         for prefix in fileter_by_prefixes:
-            resp_iter = paginator.paginate(
-                Bucket=bucket,
-                Prefix=prefix
-            )
-            result = resp_iter.build_full_result()
-            del resp_iter
+            s3objsiter = get_s3objsiter(bucket, prefix=prefix)
+            mail_body = construct_mailbody(s3objsiter)
+    else:
+        s3objsiter = get_s3objsiter(bucket)
+        mail_body = construct_mailbody(s3objsiter)
 
-            for v in result.values():
-                for d in v:
-                    mail_body += "Key: {0}, Size: {1} bytes\n".format(d["Key"], d["Size"])
-        mail_body += "\nend\n"
-        subject = 'mail subject'
+    mail_body += "\nend\n"
+    body = body_header + mail_body
+    subject = 'mail subject'
 
-        result = {
-            "Result": {
-                "subject": subject,
-                "to_addr": TO_ADDR,
-                "body": mail_body,
-                "status_code": 200
-            }
+    result = {
+        "Result": {
+            "subject": subject,
+            "to_addr": TO_ADDR,
+            "body": body,
+            "status_code": 200
         }
-        return result
+    }
+    return result
